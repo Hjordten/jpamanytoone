94870c94cb05d3f5fa0730c2b97fcbc81ca9ed9c
Hjordten <andr314e@stud.kea.dk>
2023-09-07 13:53:17 +0200

    a

--- src/test/java/com/example/jpamanytoone/controller/RegionRestControllerTest.java
+++ src/test/java/com/example/jpamanytoone/controller/RegionRestControllerTest.java
@@ -1,14 +1,29 @@
 package com.example.jpamanytoone.controller;
 
+import com.example.jpamanytoone.model.Region;
+import com.example.jpamanytoone.repository.RegionRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+@DataJpaTest
 class RegionRestControllerTest {
 
+    @Autowired
+    RegionRepository regionRepository;
     @Test
     void getRegioner() {
+        Region region = new Region();
+        region.setHref("test");
+        region.setNavn("test");
+        region.setKode("1234");
+
+        regionRepository.findByKode("1234");
+
+        Assertions.assertEquals(region.getKode(), "1234");
 
 
     }
